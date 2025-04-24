8dafa5076907d1a729dfadbdc994555b54de6e2f
mrXrobot26 <zezoelbessa10@gmail.com>
2025-04-24 19:39:06 +0200

    feat(user): wrap user controller functions with asyncHandler for improved error handling

--- controller/userController.js
+++ controller/userController.js
@@ -1,39 +1,40 @@
 import { PrismaClient } from "@prisma/client";
+import asyncHandler from 'express-async-handler';
 const prisma = new PrismaClient();
 
-const createUser = async (req, res) => {
-      const { name, email, role, password } = req.body;
+const createUser = asyncHandler(async (req, res) => {
+    const { name, email, role, password } = req.body;
     const user = await prisma.user.create({
-      data: { name, email, role, password },
+        data: { name, email, role, password },
     });
     res.json(user);
-  };
+});
 
-const getAllUsers = async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.status(200).json(users);
-};
+const getAllUsers = asyncHandler(async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+});
 
-const getUserById = async (req, res) => {
-  const user = await prisma.user.findFirst({
-    where: { id: Number(req.params.id) },
-  });
-  res.status(200).json(user);
-};
+const getUserById = asyncHandler(async (req, res) => {
+    const user = await prisma.user.findFirst({
+        where: { id: Number(req.params.id) },
+    });
+    res.status(200).json(user);
+});
 
-const updateUser = async (req, res) => {
-  const user = await prisma.user.update({
-    where: { id: Number(req.params.id) },
-    data: req.body,
-  });
-  res.status(200).json(user);
-};
+const updateUser = asyncHandler(async (req, res) => {
+    const user = await prisma.user.update({
+        where: { id: Number(req.params.id) },
+        data: req.body,
+    });
+    res.status(200).json(user);
+});
 
-const deleteUser = async (req, res) => {
-  const user = await prisma.user.delete({
-    where: { id: Number(req.params.id) },
-  });
-  res.status(200).json({ message: "user deleted successfully" });
-};
+const deleteUser = asyncHandler(async (req, res) => {
+    const user = await prisma.user.delete({
+        where: { id: Number(req.params.id) },
+    });
+    res.status(200).json({ message: "user deleted successfully" });
+});
 
 export { createUser, getAllUsers, getUserById, updateUser, deleteUser };
