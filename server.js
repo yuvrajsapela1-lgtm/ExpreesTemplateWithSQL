14b2dbe2a36339b46dcd429775411c1a46ab1a0b
mrXrobot26 <zezoelbessa10@gmail.com>
2025-04-24 16:29:47 +0200

    feat(user): implement CRUD operations for user management

--- server.js
+++ server.js
@@ -23,11 +23,43 @@ app.post("/createUser", async (req, res) => {
   res.json(user);
 });
 
+app.get("/getAllUsers", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Failed to fetch users", error);
+    res.status(500).json({ message: "Failed to fetch users" });
+  }
+});
+
+app.get("/getUser/:id", async (req, res) => {
+  const user = await prisma.user.findFirst({
+    where: { id: Number(req.params.id) },
+  });
+  res.status(200).json(user);
+});
+
+app.put("/updateUser/:id", async (req, res) => {
+  const user = await prisma.user.update({
+    where: { id: Number(req.params.id) },
+    data: req.body,
+  });
+  res.status(200).json(user);
+});
+
+app.delete("/deleteUser/:id", async (req, res) => {
+  const user = await prisma.user.delete({
+    where: { id: Number(req.params.id) },
+  });
+  res.status(200).json({ message: "user deleted sucessfuly" });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-// if the route you send is not found | solve html problem
+//  solve html problem
 app.use((req, res, next) => {
   const err = new Error(`Can't find ${req.originalUrl} on this server`);
   err.status = 404;
